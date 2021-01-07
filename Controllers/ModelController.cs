6583311b2d5fcc6316f08d96c76763ea16ed7aab
OZGUR YILDIZ <ozgur.yildiz@akgida.com.tr>
2021-01-07 17:24:31 +0300

    Proje dosyası ekle.

---
+++ Controllers/ModelController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EnvanterYonetimi.Models;
+using EnvanterYonetimi.Models.Entity;
+
+namespace EnvanterYonetimi.Controllers
+{
+    public class ModelController : Controller
+    {
+        // GET: Model
+        envanterTakipWebEntities database = new envanterTakipWebEntities(); // Database bağlantı nesnesi tanımlandı
+        public ActionResult Index()
+        {
+            var modelsonuc = database.Modeller.ToList();
+            return View(modelsonuc);
+        }
+    }
+}
