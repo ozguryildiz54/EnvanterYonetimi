7f7b412abae16c6a567cbc72f3e3ac84a04acf31
ozguryildiz54 <ozguryildiz.ada@gmail.com>
2021-02-14 23:24:54 +0300

    Ürün Kaydı Sayfasının Tasarımı Tamamlandı.

---
+++ Controllers/DepoIslemleri/UrunKaydiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnvanterYonetimi.Controllers.DepoIslemleri
+{
+    public class UrunKaydiController : Controller
+    {
+        // GET: UrunKaydi
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string degiskenler)
+        {
+            
+            return View();
+        }
+    }
+}
--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.DirectoryServices;
 using System.DirectoryServices.Protocols;
-using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace EnvanterYonetimi.Controllers
 {
@@ -13,48 +10,54 @@ namespace EnvanterYonetimi.Controllers
     {
         #region LoginUserCheck
 
-        public bool ValidateUser(string userName, string password)
-        {
-            bool validation;
-            try
+            public bool ValidateUser(string userName, string password)
             {
-                LdapConnection ldc = new LdapConnection(new LdapDirectoryIdentifier((string)null, false, false));
-                NetworkCredential nc = new NetworkCredential(userName, password, "AKGIDA");
-                ldc.Credential = nc;
-                ldc.AuthType = AuthType.Negotiate;
-                ldc.Bind(nc); // user has authenticated at this point, as the credentials were used to login to the dc.
-                validation = true;
-            }
-            catch (LdapException)
-            {
-                validation = false;
-            }
-            return validation;
+                bool validation;
+                try
+                {
+                    LdapConnection ldc = new LdapConnection(new LdapDirectoryIdentifier((string)null, false, false));
+                    NetworkCredential nc = new NetworkCredential(userName, password, "AKGIDA");
+                    ldc.Credential = nc;
+                    ldc.AuthType = AuthType.Negotiate;
+                    ldc.Bind(nc); // user has authenticated at this point, as the credentials were used to login to the dc.
+                    validation = true;
+                }
+                catch (LdapException)
+                {
+                    validation = false;
+                }
+                return validation;
         }
         #endregion
-
 
         // GET: Login
         [HttpGet]
         public ActionResult Index()
+        {           
+           return View();
+        }
+        public ActionResult Error()
         {
-
-            String sonuc = "kod çalışmıyor";
-            sonuc = ValidateUser("ozgdfgdfgur.yildiz", "Kasim.hdedd2019").ToString();
-            ViewData["sonuc"] = sonuc;
             return View();
         }
-
-        //[HttpPost]
-        //public string PostLogin(string email, string pass)
-        //{
-        //    String sonuc = "kod çalışmıyor";
-        //    sonuc = ValidateUser("ozgur.yildiz", "Kasim.2019").ToString();
-        //    ViewData["sonuc"] = sonuc;
-        //    return sonuc;
-        //}
-
-        ////OU=AKGIDA,DC=akgida,DC=com,DC=tr
-        ////LDAP://www.akgida.com.tr/OU=AKGIDA,O=ON
+        [HttpPost]
+        public ActionResult Index(string eposta, string sifre)
+        {
+            ViewData["mesaj"] = "calismiyor";
+            eposta = Request.Form["email"];
+             sifre = Request.Form["pass"];
+            TempData["kullaniciAdi"] = eposta;
+            String sonuc = "kod çalışmıyor";
+            sonuc = ValidateUser(eposta, sifre).ToString();
+            ViewData["sonuc"] = sonuc;
+            if (sonuc == "True")
+            {
+                ViewData["mesaj"] = "true";
+                Response.Redirect("/Main/Index", true);
+            }
+            ViewData["mesaj"] = "false";
+            return View();
+        }
+       
     }
 }
