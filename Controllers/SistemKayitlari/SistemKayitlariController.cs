7b62f51e34bfff7e5d130dba54476eb9214bd1a6
ozguryildiz54 <ozguryildiz.ada@gmail.com>
2021-02-17 15:57:57 +0300

    Menü linkleri tanımlandı

---
+++ Controllers/SistemKayitlari/SistemKayitlariController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnvanterYonetimi.Controllers.SistemKayitlari
+{
+    public class SistemKayitlariController : Controller
+    {
+        // GET: SistemKayitlari
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
